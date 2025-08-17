52b8d222435e63106c0fee6c22ba6419a3108b91
Eng_3bdo_7amed <3bdo.7amed.com@gmail.com>
2025-08-17 03:05:38 +0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <h1 className="font-bold text-4xl">Welcome to My Portfolio</h1>
+        <p className="text-lg">
+          I am a web developer with a passion for creating beautiful and
+          functional websites.
+        </p>
+      </main>
+    </div>
+  );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Navbar() {
+  return <div className="bg-gray-800 text-danger p-4">Navbar</div>;
+}
